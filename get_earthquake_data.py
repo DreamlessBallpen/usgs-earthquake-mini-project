6c10cfa3ccc50b8eaabf1681df8e5bf84d047041
Fitz Pudadera <107037118+DreamlessBallpen@users.noreply.github.com>
2025-04-06 18:42:10 +0800

    Adding the .gitignore file for clean syncs, and the initial API request, checking the status code

---
+++ get_earthquake_data.py
@@ -0,0 +1,19 @@
+# Part1: Get earthquake data from the website, and parse items
+
+# This is the website it feeds into: https://earthquake.usgs.gov/earthquakes/map
+
+import requests
+
+endpoint = r"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+response = requests.get(endpoint)
+
+# Part1-1: Examine the status, if the request was successful. Examine the data structure as well.
+
+if response.status_code == 200:
+    data = response.json()
+    print("No issues. Printing the keys from our JSON data")
+    print(data.keys())
+else:
+    print(f"Error in fetching data: {response.status_code}")
+    
+
